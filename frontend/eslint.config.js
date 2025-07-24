49bc8a23f1935981584b3b1465b6536f8f9fd5dd
Sagi Menahem <sagiia1997@gmail.com>
2025-07-24 18:36:28 +0300

    Updating VS plugins

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// frontend/eslint.config.js
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config(
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        ...pluginReactConfig,
+        settings: {
+            react: { version: 'detect' },
+        },
     },
-  },
-])
+
+    {
+        files: ['**/*.{jsx,tsx}'],
+        ...jsxA11y.configs.recommended,
+    },
+
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+        },
+        plugins: {
+            import: importPlugin,
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+                    pathGroups: [
+                        { pattern: 'react', group: 'external', position: 'before' },
+                        { pattern: '{components,constants,context,hooks,pages,services,types}/**', group: 'internal' },
+                    ],
+                    pathGroupsExcludedImportTypes: ['react'],
+                    'newlines-between': 'always',
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                },
+            ],
+        },
+    },
+
+    eslintConfigPrettier
+);
