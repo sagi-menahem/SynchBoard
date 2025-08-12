894cf8ae64111f1db354e25d7980a37e1a4d276c
Sagi Menahem <sagiia1997@gmail.com>
2025-08-12 12:35:53 +0300

    refactor: update AppProperties to use @Component instead of @Configuration

--- backend/src/main/java/io/github/sagimenahem/synchboard/BackendApplication.java
+++ backend/src/main/java/io/github/sagimenahem/synchboard/BackendApplication.java
@@ -2,8 +2,12 @@ package io.github.sagimenahem.synchboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import io.github.sagimenahem.synchboard.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/io/github/sagimenahem/synchboard/config/AppProperties.java
+++ backend/src/main/java/io/github/sagimenahem/synchboard/config/AppProperties.java
@@ -1,9 +1,9 @@
 package io.github.sagimenahem.synchboard.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
 
