b3f26c35c24f9ddc9122a90b7c9a06ae827dc52d
Sagi Menahem <sagiia1997@gmail.com>
2025-08-20 18:33:55 +0300

    style: Format code for consistency and readability in Input, WebSocketProvider, websocketService, and vite.config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    define: {
-        global: 'globalThis',
-    },
+  plugins: [react(), tsconfigPaths()],
+  define: {
+    global: 'globalThis',
+  },
 });
