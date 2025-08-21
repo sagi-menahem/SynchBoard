37454821cd3b441dcc79106c24a1084b3d25fc31
Sagi Menahem <sagiia1997@gmail.com>
2025-08-21 18:38:38 +0300

    refactor: Standardize import paths for services and utilities

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,7 +1,7 @@
 export * from './CanvasUtils';
 export * from './DateUtils';
 export * from './ErrorUtils';
-export { default as Logger } from './Logger';
+export { default as Logger } from './logger';
 export * from './TypeGuards';
 export * from './UnifiedValidation';
 
