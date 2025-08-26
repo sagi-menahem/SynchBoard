3511c5bd9637ebc460491094adb260627d750809
Sagi Menahem <sagiia1997@gmail.com>
2025-08-26 03:13:15 +0300

    feat: Enhance HeaderToolbar with improved styling, tool controls, and localization updates

--- frontend/src/components/board/workspace/index.ts
+++ frontend/src/components/board/workspace/index.ts
@@ -1,5 +1,5 @@
 export { default as BoardWorkspace } from './BoardWorkspace';
 export { default as Canvas } from './Canvas';
 export { HeaderToolbar } from './HeaderToolbar';
-export { LineToolsGroup } from './LineToolsGroup';
+export { LineToolsDropdown } from './LineToolsDropdown';
 export { ShapeToolsDropdown } from './ShapeToolsDropdown';
