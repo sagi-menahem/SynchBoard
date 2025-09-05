6e45ef550f2247b8511482024fa40e953a602a6a
Sagi Menahem <sagiia1997@gmail.com>
2025-09-05 14:04:09 +0300

    refactor: remove obsolete SQL session file

--- Synchboard Local.session.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE action_history CASCADE;
-DROP TABLE board_objects CASCADE;
-DROP TABLE group_boards CASCADE;
-DROP TABLE group_members CASCADE;
-DROP TABLE messages CASCADE;
-DROP TABLE users CASCADE;
-DROP TABLE pending_registrations CASCADE;
